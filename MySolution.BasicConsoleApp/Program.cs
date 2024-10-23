db33deca210b710f16612dd7c0b985f32a4dfa9b
ongzhixian <zhixian@hotmail.com>
2024-10-24 00:09:45 +0800

    init commit

---
+++ MySolution.BasicConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+CancellationToken cancellationToken =  cancellationTokenSource.Token;
+
+Console.CancelKeyPress += Console_CancelKeyPress;
+
+void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+{
+    e.Cancel = true;
+    cancellationTokenSource.Cancel();
+}
+
+Console.WriteLine("[PROGRAM START]");
+
+while (!cancellationToken.IsCancellationRequested)
+{
+    Console.WriteLine("[{0:HH:mm:ss}] Ok", DateTime.UtcNow);
+    Thread.Sleep(1000);
+}
+
+Console.WriteLine("[PROGRAM END]");
